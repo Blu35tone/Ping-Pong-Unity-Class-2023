59ce6fd751a3d0f85a5aa69b6354e7eeb1c8e433
Mashley <65775902+Blu35tone@users.noreply.github.com>
2023-12-09 17:44:24 -0500

    This Game is Complete

---
+++ Ping Pong Game/Assets/Script/Left Paddle.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftPaddle : MonoBehaviour
+{
+    //variable for movement
+    public float speed = 0.0325f;
+    public float yBorder = 4.5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) && transform.position.y < yBorder)    //when O is pressed
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y + speed);   //move up
+        }
+
+        if (Input.GetKey(KeyCode.S) && transform.position.y > -yBorder)    //when L is pressed
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y - speed); //move down
+        }
+    }
+}
